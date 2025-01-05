3aa368a7661f21f2bbe438ce6360db530f0cbdad
Leokhaidem <leokhaidemgit@gmail.com>
2025-01-05 08:42:36 +0530

    add order-table and hooks

--- src/pages/orderPage/OrderPage.jsx
+++ src/pages/orderPage/OrderPage.jsx
@@ -5,14 +5,29 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { Badge } from "@/components/ui/badge";
 import axios from "axios";
 import { useOrders } from "@/features/orders/hooks/useOrders";
+import { useUpdateOrderStatus } from "@/features/orders/hooks/useUpdateOrderStatus";
 
 export function OrderPage() {
 
   const { data: orders, isLoading, error } = useOrders();
-  console.log(orders);
+  // const updateOrderStatus = useUpdateOrderStatus();
 
   if (isLoading) return <p>Loading orders...</p>;
   if (error) return <p>Error loading orders: {error.message}</p>;
+
+  // const handleStatusChange = (orderId, status) => {
+  //   if (!status) return;
+  //   updateOrderStatus.mutate({ orderId, status },
+  //     {
+  //       onError: (error) => {
+  //         alert(`Error: ${error.message}`);
+  //       },
+  //       onSuccess: () => {
+  //         alert("Order status updated successfully.");
+  //       }
+  //     }
+  //   );
+  // };
 
   return (
     <div className="space-y-4">
@@ -32,18 +47,18 @@ export function OrderPage() {
         </TableHeader>
         <TableBody>
           {orders.map((order) => (
-            <TableRow key={order.id}>
-              <TableCell>{order.id}</TableCell>
-              <TableCell>{order.customerName}</TableCell>
-              <TableCell>{new Date(order.orderDate).toLocaleDateString()}</TableCell>
+            <TableRow key={order._id}>
+              <TableCell>{order._id}</TableCell>
+              <TableCell>{order.shipping_address.full_name}</TableCell>
+              <TableCell>{new Date(order.createdAt).toLocaleDateString()}</TableCell>
               <TableCell>
                 <Badge variant={order.status === "Pending" ? "warning" : "success"}>
                   {order.status}
                 </Badge>
               </TableCell>
-              <TableCell>${order.totalAmount.toFixed(2)}</TableCell>
+              <TableCell>₹{order.amount.toFixed(2)}</TableCell>
               <TableCell>
-                <Select onValueChange={(status) => handleStatusChange(order.id, status)}>
+                <Select onValueChange={(status) => handleStatusChange(order._id, status)}>
                   <SelectTrigger>
                     <SelectValue placeholder="Update Status" />
                   </SelectTrigger>

4d0617ad0606cf1a9a392be67eaaec66a1b686df
alsonwangkhem <alsonwangkhem20092@gmail.com>
2025-01-22 16:55:26 +0530

    routes 22

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -17,8 +17,8 @@ import ProductCreate from "@/pages/super_admin/productCreate/ProductCreate";
 import { Analytics } from "@/pages/super_admin/analytics";
 import ProductUpdate from "@/pages/super_admin/productUpdate/ProductUpdate";
 import CategoryCreate from "@/pages/super_admin/categoryCreate/CategoryCreate";
-import AddSubCategory from "@/pages/super_admin/categoryCreate/AddSubCategory"; // Import AddSubCategory
-import EditSubCategory from "@/pages/super_admin/categoryUpdate/EditSubCategory"; // Import EditSubCategory
+import AddSubCategory from "@/pages/super_admin/categoryCreate/AddSubCategory";
+import EditSubCategory from "@/pages/super_admin/categoryUpdate/EditSubCategory";
 import { ShopPage } from "@/pages/super_admin/shopPage/ShopPage";
 import ShopDetail from "@/pages/super_admin/shopDetail/ShopDetail";
 import ShopCreate from "@/pages/super_admin/shopCreate/ShopCreate";
@@ -28,41 +28,43 @@ import useAuthenticationStore from "@/store/useAuthenticationStore";
 import { RedirectAuthenticatedUser } from "./RedirectAuthenticatedUser";
 import SellerProductList from "@/pages/seller_admin/sellerProductList/SellerProductList";
 import SellerShop from "@/pages/seller_admin/sellerShop/SellerShop";
+
 export default function AppRoutes() {
   const { user } = useAuthenticationStore();
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
+        {/* Protected Routes */}
         <Route element={<ProtectedRoute />}>
           <Route element={<SideBarLayout />}>
+            {/* Common Routes */}
+            <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
             <Route path={ROUTES.PRODUCT.CREATE} element={<ProductCreate />} />
             <Route path={ROUTES.SHOP.CREATE} element={<ShopCreate />} />
             <Route path={ROUTES.PRODUCT.DETAIL} element={<ProductDetail />} />
-            {user?.role == "Super_Admin" && (
-              <Route>
-                <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+            <Route path={ROUTES.ORDERPAGE} element={<OrderPage />} />
+            <Route path={ROUTES.SPECIFICORDER} element={<OrderDetail />} />
+
+            {/* Super Admin Routes */}
+            {user?.role === "Super_Admin" && (
+              <>
                 <Route path={ROUTES.ANALYTICS} element={<Analytics />} />
-                <Route path={ROUTES.ORDERPAGE} element={<OrderPage />} />
-                <Route path={ROUTES.SPECIFICORDER} element={<OrderDetail />} />
                 <Route path={ROUTES.ALLCUSTOMERS} element={<CustomerPage />} />
                 <Route
                   path={ROUTES.SPECIFICCUSTOMER}
                   element={<CustomerDetail />}
-                />
-
-                <Route path={ROUTES.PRODUCT.LIST} element={<ProductPage />} />
-                <Route
-                  path={ROUTES.PRODUCT.UPDATE}
-                  element={<ProductUpdate />}
                 />
                 <Route path={ROUTES.ALLCATEGORIES} element={<CategoryPage />} />
                 <Route
                   path={ROUTES.SPECIFICCATEGORY}
                   element={<CategoryDetail />}
                 />
+                <Route path={ROUTES.EDITCATEGORY} element={<CategoryUpdate />} />
+                <Route path={ROUTES.PRODUCT.LIST} element={<ProductPage />} />
                 <Route
-                  path={ROUTES.EDITCATEGORY}
-                  element={<CategoryUpdate />}
+                  path={ROUTES.PRODUCT.UPDATE}
+                  element={<ProductUpdate />}
                 />
                 <Route
                   path={ROUTES.CATEGORY_CREATE}
@@ -78,23 +80,23 @@ export default function AppRoutes() {
                 />
                 <Route path={ROUTES.SHOP.LIST} element={<ShopPage />} />
                 <Route path={ROUTES.SHOP.DETAIL} element={<ShopDetail />} />
-              </Route>
+              </>
             )}
+
+            {/* Shop Seller Admin Routes */}
             {user?.role === "Shop_Seller_Site_Admin" && (
-              <Route>
-                <Route
-                  path={ROUTES.SELLER_DASHBOARD}
-                  element={<SellerDashboard />}
-                />
+              <>
                 <Route
                   path={ROUTES.SELLER_PRODUCT}
                   element={<SellerProductList />}
                 />
                 <Route path={ROUTES.SELLER_SHOP} element={<SellerShop />} />
-              </Route>
+              </>
             )}
           </Route>
         </Route>
+
+        {/* Authentication Routes */}
         <Route element={<RedirectAuthenticatedUser />}>
           <Route path={ROUTES.LOGIN} element={<Login />} />
           <Route path={ROUTES.SIGNUP} element={<Signup />} />

1e649403e04b2d523fd2d99e915d13ae7c87a70d
nganaremba-rem <ngancloverr+remkugithub@gmail.com>
2025-01-27 20:40:12 +0530

    Changed the cloudinary account to ema's cloudinary account, product variants is mandatory now, subcategory is not mandatory and is fixed now, many other bugs fixed

--- src/layouts/SidebarLayout.jsx
+++ src/layouts/SidebarLayout.jsx
@@ -77,7 +77,7 @@ export default function SideBarLayout() {
                     onClick={logout}
                     disabled={isPending}
                   >
-                    Hera Logout
+                    Logout
                   </DropdownMenuItem>
                 </DropdownMenuContent>
               </DropdownMenu>
